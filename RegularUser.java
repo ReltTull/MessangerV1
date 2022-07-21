82e7204aad77b489f2990f11e7042d78a3855bcf
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-21 18:18:22 +0300

    renames

--- RegularUser.java
+++ RegularUser.java
@@ -6,7 +6,7 @@ public class RegularUser extends User{
     }
 
     @Override
-    TextMessage fillingTextMessage() {
+    TextMessagesStorage fillingTextMessage() {
         return super.fillingTextMessage();
     }
 
